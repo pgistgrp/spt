12c88c51fcae197d6f9fffa8ed726b79b086e2d1
Mary Roderick <roderimj@uw.edu>
2015-08-29 15:05:49 -0400

    updated login

--- spt/view/Application.js
+++ spt/view/Application.js
@@ -15,11 +15,53 @@ Ext.define('SPT.view.Application', {
 	],
 	
 initComponent: function () { 
-	//need to pass appPanel to children bc the child views treat the window and not appPanel as parent
-	//tried all other variations from child with no luck: ownerCt traversal, up, getCmp, getComponent, findParentByType 
-	var me = this; 
 	
-	this.login();
+	var loginStore = Ext.create('SPT.store.SPTUser');
+	loginStore.addListener('load',this.openApp, this);
+	
+	this.login(loginStore);
+
+},
+
+login: function(loginStore) {
+	
+	 var username; 
+	 var token = null;
+	 
+	 //Check to see if CSFUser
+	 var cookie = Ext.util.Cookies.get('csfuser');
+	 if (cookie != null){
+		username = cookie;
+	 	loginStore.getProxy().url =  loginStore.getProxy().url + username +'/' + token + '/'+ 'csf';
+	 }else{
+		//username = CG.global.Env.authuser;
+	    //token = CG.global.Env.token;
+		//apihost = window.location.host;
+		username = 'roderimj';
+   	token = 'cybergis_token_Aqxd696aFH3ZcX1l';
+   	apihost = encodeURIComponent('sandbox.cigi.illinois.edu:443');
+   	loginStore.getProxy().url =  loginStore.getProxy().url + username +'/' + token + '/'+ apihost + '/' + 'cybergis';
+	 }
+			
+	loginStore.load(function(records, operation, success) {
+		console.log('load user');
+		var record = loginStore.getAt(0);
+		if(! record.get('successful')){
+			  Ext.Msg.show({
+                 title: 'Login failed',
+                 msg: record.get('error'),
+                 buttons: Ext.Msg.OK
+             });	
+		}else{
+			record.set('username', username);
+		};
+	});
+},
+
+openApp: function(){
+	//need to pass appPanel to children bc the child views treat the window and not appPanel as parent
+	//tried all other variations from child with no luck: ownerCt traversal, up, getCmp, getComponent, findParentByType
+	var me = this; 
 	
 	var win = Ext.create('Ext.window.Window', {
 		id: 'cg_pgist_win',
@@ -48,41 +90,7 @@ initComponent: function () {
   });
 	
 	    win.show();
-},
-
-login: function() {
 	
-	 var username; 
-	 var token = null;
-	 var loginStore = Ext.create('SPT.store.SPTUser');
-	 //Check to see if CSFUser
-	 var cookie = Ext.util.Cookies.get('csfuser');
-	 if (cookie != null){
-		username = cookie;
-	 	loginStore.getProxy().url =  loginStore.getProxy().url + username +'/' + token + '/'+ 'csf';
-	 }else{
-		//username = CG.global.Env.authuser;
-	    //token = CG.global.Env.token;
-		//apihost = window.location.host;
-		username = 'roderimj';
-   	token = 'cybergis_token_B5FH6EDHZdvb8gV1';
-   	apihost = encodeURIComponent('sandbox.cigi.illinois.edu:443');
-   	loginStore.getProxy().url =  loginStore.getProxy().url + username +'/' + token + '/'+ apihost + '/' + 'cybergis';
-	 }
-			
-	loginStore.load(function(records, operation, success) {
-		console.log('load user');
-		var record = loginStore.getAt(0);
-		if(! record.get('successful')){
-			  Ext.Msg.show({
-                 title: 'Login failed',
-                 msg: record.get('error'),
-                 buttons: Ext.Msg.OK
-             });	
-		}else{
-			record.set('username', username);
-		};
-	});
 },
 
 getCurrentWorkflowInfo: function(){
